5a351f912a3b7dc617ddef7902cb0edff768ced5
Guillermo Lillo <Guillermo.jobs@hotmail.com>
2021-05-18 09:59:38 +0200

    añadido el selector de img y de idioma en Cursos

--- src/app/pages/cursos/components/edit-curso/edit-curso.component.ts
+++ src/app/pages/cursos/components/edit-curso/edit-curso.component.ts
@@ -2,6 +2,8 @@ import { CursosService } from '../../cursos.service';
 import { Component, EventEmitter, Input, OnInit, Output, OnDestroy } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { Curso } from 'src/app/models/curso';
+import { UploadFilesService } from 'src/app/services/upload-files.service';
+import { DomSanitizer } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-edit-curso',
@@ -11,14 +13,21 @@ export class EditCursoComponent implements OnInit {
 
   @Input() curso!:Curso;
   @Output("close") editModeEmiter = new EventEmitter<Curso>();
+
   cursoForm!:FormGroup;
+  idiomas: string[] = ["español","ingles","frances"]
 
-  constructor(private cursosSer:CursosService) { }
+  provisionalFile!:any;
+  uploadFile!:File;
+  uploadFileURl!:any
 
-  ngOnInit(): void {
+  constructor(private cursosSer:CursosService, private upFilesSer:UploadFilesService, private sanitizer:DomSanitizer) { }
+
+  ngOnInit(): void {    
     this.cursoForm = new FormGroup({
       "name" : new FormControl(this.curso.name, [Validators.required]),
       "description" : new FormControl(this.curso.description || ""),
+      "idioma" : new FormControl(this.curso.idioma, [Validators.required]),
       "image" : new FormControl(this.curso.image || "../../assets/defaultIMG.svg"),
       "time": new FormGroup({
         "hours" : new FormControl(this.curso.hours || 0),
@@ -35,31 +44,54 @@ export class EditCursoComponent implements OnInit {
   }
 
   onSubmit(){
+    this.uploadImg().then(() =>{
 
-    this.curso = new Curso(
-      this.curso.id,
-      this.cursoForm.value.name,
-      this.curso.idioma,
-      this.curso.actividades,
-      this.cursoForm.value.description,
-      this.cursoForm.get("time.hours")!.value,
-      this.cursoForm.get("time.minutes")!.value,
-      this.cursoForm.value.image,
-    );
+      this.curso = new Curso(
+        this.curso.id,
+        this.cursoForm.value.name,
+        this.curso.idioma,
+        this.curso.actividades,
+        this.cursoForm.value.description,
+        this.cursoForm.get("time.hours")!.value,
+        this.cursoForm.get("time.minutes")!.value,
+        this.uploadFileURl,
+      );
 
-    if(!this.curso.id)
-      this.cursosSer.addCurso(this.curso);
-    else
-      this.cursosSer.editCurso(this.curso);
-    
-    this.editModeEmiter.emit(this.curso);
+      if(!this.curso.id)
+        this.cursosSer.addCurso(this.curso);
+      else
+        this.cursosSer.editCurso(this.curso);
+      
+      this.editModeEmiter.emit(this.curso);
+    });
   }
 
   deleteCurso(){
+    this.upFilesSer.deleteImg("cursos",this.curso.id);
+
     if(!this.curso.id)
       this.cursosSer.deleteNullCurso(this.curso)
     else
       this.cursosSer.deleteCurso(this.curso);
+  }
+
+  cambiarImg(event:any){
+    this.uploadFile = event.files[0];
+
+    if(!event.files[0]){return}
+
+    var reader = new FileReader();
+    reader.readAsDataURL(event.files[0]);
+
+    reader.onloadend = () => {
+      this.provisionalFile = this.sanitizer.bypassSecurityTrustUrl(`${reader.result}`);
+    }
+  }
+
+  async uploadImg(){
+    await this.upFilesSer.uploadImg(this.uploadFile,"cursos",this.curso.id).then(() =>
+      this.upFilesSer.downloadImg("cursos",this.curso.id).then((url) => this.uploadFileURl = url)
+    )
   }
 
 }
